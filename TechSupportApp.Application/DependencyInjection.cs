e55efef87644639d70cbe6ab33b5af85adbf9168
Лобанов Кирилл <lobanov.k.e@gmail.com>
2020-12-01 16:56:19 +0500

    Добавьте файлы проекта.

---
+++ TechSupportApp.Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechSupportApp.Application
+{
+    public static class DependencyInjection
+    {
+        public static void AddApplication(this IServiceCollection services)
+        {
+
+        }
+    }
+}
---
+++ TechSupportApp.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechSupportApp.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static void AddInfrastructure(this IServiceCollection services)
+        {
+            
+        }
+    }
+}
